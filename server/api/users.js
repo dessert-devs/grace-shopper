db2e1b395970540ca816a6c24aab67f41ec9a42d
Yufan Zhang <mikyla.c.zhang@gmail.com>
2020-11-11 23:10:48 -0500

    Feature: Set up post, put and delete routes for pending orders of a user

--- server/api/users.js
+++ server/api/users.js
@@ -38,6 +38,85 @@ router.get('/:userId/pending-order', async (req, res, next) => {
   }
 })
 
+router.post('/:userId/pending-order', async (req, res, next) => {
+  try {
+    // if(req.body.firstName===undefined || req.body.lastName === undefined || req.body.email===undefined){
+    //   res.status(500).json('Fields First Name, Last Name and Email are required!')
+    // }
+    const addedCart = await Order.findOrCreate({
+      where: {userId: req.params.userId, pending: true}
+    })
+    const order_id = addedCart[0].dataValues.id
+    console.log(order_id)
+    const {amount, price_per_item, total_price, product_name} = req.body
+    const product = await Product.findAll({
+      where: {
+        name: product_name
+      }
+    })
+    const product_id = product[0].dataValues.id
+    // console.log('here is full product: ', product)
+    // const {dataValues} = product[0]
+    // const product_id = dataVales.id
+    // console.log('here is product id: ')
+    const addedDetail = await Order_Product.create({
+      orderId: order_id,
+      amount,
+      price_per_item,
+      total_price,
+      productId: product_id
+    })
+    res.json(addedDetail)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//we need the front end to have product Id
+router.put('/:userId/pending-order/:productId', async (req, res, next) => {
+  try {
+    const order = await Order.findAll({
+      where: {userId: req.params.userId}
+    })
+    console.log('order here: ', order)
+    const order_id = order[0].dataValues.id
+    console.log('order id here: ', order_id)
+    const order_product = await Order_Product.findAll({
+      where: {
+        orderId: order_id,
+        productId: req.params.productId
+      }
+    })
+    console.log('order product here: ', order_product)
+    await order_product[0].update(req.body)
+    const updated = await Order_Product.findAll({
+      where: {
+        orderId: order_id,
+        productId: req.params.productId
+      }
+    })
+    res.json(updated[0])
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:userId/pending-order/:productId', async (req, res, next) => {
+  try {
+    const order = await Order.findAll({userId: req.params.userId})
+    const order_id = order[0].dataValues.id
+    const order_product = await Order_Product.findAll({
+      where: {
+        orderId: order_id,
+        productId: req.params.productId
+      }
+    })
+    await order_product[0].destroy()
+  } catch (error) {
+    next(error)
+  }
+})
+
 /*
 ===========For Admin==========
 we should also include '/delete'
